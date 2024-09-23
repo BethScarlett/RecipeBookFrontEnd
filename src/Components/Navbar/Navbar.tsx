c4cff748d7e54e9db451842c6794ddd2e261eb9f
Bethany Burrows <jedcburrows@gmail.com>
2024-09-23 09:14:42 +0100

    Setup basic components for the navbar and the burger menu.

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Recipe Book</h1>
+      <BurgerMenu />
+    </div>
+  );
+};
+
+export default Navbar;
