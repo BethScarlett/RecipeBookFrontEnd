ca9a094f3064b8217092e1147651aaefbec42611
Bethany Burrows <jedcburrows@gmail.com>
2024-09-27 09:28:38 +0100

    Added data file containing recipes and mapped this data onto cards.

--- src/Components/RecipeCard/RecipeCard.tsx
+++ src/Components/RecipeCard/RecipeCard.tsx
@@ -1,15 +1,25 @@
 import "./RecipeCard.scss";
 import placeholderImg from "../../assets/Images/food-placeholder.png";
 
-const RecipeCard = () => {
+type RecipeCardProps = {
+  name: string;
+  description: string;
+  category: string;
+};
+
+const RecipeCard = ({ name, description, category }: RecipeCardProps) => {
   return (
     <div className="recipe-card">
-      <h2 className="recipe-card__heading">Recipe Name</h2>
-      <img className="recipe-card__image" src={placeholderImg} alt="Image of dish" />
-      <p className="recipe-card__description">"A short description of the dish"</p>
-      <p className="recipe-card__suitable">Suitable for: These people, and those ones too</p>
+      <h2 className="recipe-card__heading">{name}</h2>
+      <img
+        className="recipe-card__image"
+        src={placeholderImg}
+        alt="Image of dish"
+      />
+      <p className="recipe-card__description">{description}</p>
+      <p className="recipe-card__suitable">Category: {category}</p>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeCard
+export default RecipeCard;
---
+++ src/Types/Recipe.ts
@@ -0,0 +1,10 @@
+type Recipe = {
+  id: number;
+  img: string;
+  name: string;
+  description: string;
+  category: string;
+  user_id: number;
+};
+
+export default Recipe;
