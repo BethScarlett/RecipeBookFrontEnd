87b91a043994280ea8b0c58a853167406c8d0948
Bethany Burrows <jedcburrows@gmail.com>
2024-09-30 10:28:49 +0100

    Set up input box to filter recipes based on a search term inputted by the user (Commiting due to missed branch change)

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,13 +1,38 @@
+import { ChangeEvent, useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import RecipeCard from "../Components/RecipeCard/RecipeCard";
 import Recipes from "../Data/Recipes";
+import Recipe from "../Types/recipe";
 import "./HomePage.scss";
 
 const HomePage = () => {
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([
+    ...Recipes,
+  ]);
+
+  const filterByName = (searchTerm: string) => {
+    setFilteredRecipes(
+      Recipes.filter((recipe) => {
+        if (recipe.name.includes(searchTerm)) {
+          return recipe;
+        }
+      })
+    );
+    console.log(filteredRecipes);
+  };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const cleanInput: string =
+      e.currentTarget.value.slice(0, 1).toUpperCase() +
+      e.currentTarget.value.slice(1);
+    filterByName(cleanInput);
+  };
+
   return (
     <div>
       <Navbar />
-      {Recipes.map((recipe) => (
+      <input type="text" placeholder="Search ..." onChange={handleInput} />
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
           <RecipeCard
             name={recipe.name}
